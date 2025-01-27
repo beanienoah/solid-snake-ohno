d02276f01ca2fbac8803c91fe9d7b7503f237a33
Noah McCullough <noah.mccullough117@gmail.com>
2025-01-27 05:42:18 +0000

    under a weird repo cause my suggestion was bad. anyways should work fine :)

---
+++ hwk1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    const int n = 10;
+    for (int x = 0; x < n;  x++) {
+        printf("Hello world %d of %d! \n", x, n);
+    }
+    return 0;
+}
